6b3dc74d61928148fde2768dea64932de69be1a7
Martanto <martanto@live.com>
2026-09-23 14:05:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ if __name__ == "__main__":
     )
     plt.savefig("output.png", dpi=150, bbox_inches="tight")
     print("Saved to output.png")
+
+    fig = (
+        PlotAvailability("example.xlsx")
+        .select(years=["2016", "2017", "2018"])
+        .plot(
+            title="Data Availability — VG.IJEN.00.EHZ",
+            kind="bar",
+            hspace=2,
+            fig_width=10,
+            figsize_per_year=0.8,
+            cbar_height=5,
+        )
+    )
+    fig.savefig("output-bar.png", dpi=150, bbox_inches="tight")
+    print("Saved to output-bar.png")
